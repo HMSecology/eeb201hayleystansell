1c5750b7535919114da8b669e54e2ccd842316e7
Hayley Stansell <hmstansell@g.ucla.edu>
2015-11-23 11:07:09 -0800

    Begin Exercise 2

--- M200AEvoBioGeneticDriftLab.R
+++ M200AEvoBioGeneticDriftLab.R
@@ -33,5 +33,24 @@ rbinom(10, 1, 0.1)
 #
 #p= allele freq (at t)
 #N= pop size (number of diploid individs, or number of c-somes in pop)----HERE = number of diploid individs (so, pop has 2N csomes)
-
-
+#
+p<- 0.1
+N<- 10
+count<- rbinom(1,2*N, p)
+count
+#[1] 2
+count/(2*N)
+#[1] 0.1
+#
+#####################################################################################EXERCISE TWO#######################
+#
+#Write an R-function that simulates T generations of genetic drift for L independent SNPs. 
+#Keep track of allele freq's of each of the L-SNPs in each of the T-generations. 
+#All SNPs should start in the initial generation at frequency "p"
+#
+#Hint: Initialize a matrix to keep track of the freqs in each generation:
+# T<- 5 
+# L<- 3
+# freqs<- matrix(nrow=T,ncol=L) #sets up framework for matrix to get allel freqs each generation.
+#                                 each ROW is a generation, each COLUMN is a SNP
+#
